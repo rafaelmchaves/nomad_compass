31afbde1b94504871c84ab637b7568e9935e3fd8
Rafael Marques Chaves <rafaelmchaves@gmail.com>
2024-02-21 19:54:54 -0300

    Create the Cities controller with 2 endpoints
    Create '/cities?name=' and '/cities/{id}' endpoints

---
+++ src/main/java/com/nomadcompass/infrastructure/controller/CitiesController.java
@@ -0,0 +1,24 @@
+package com.nomadcompass.infrastructure.controller;
+
+import com.nomadcompass.infrastructure.request.CityResponse;
+import io.micronaut.http.HttpResponse;
+import io.micronaut.http.annotation.Controller;
+import io.micronaut.http.annotation.Get;
+import io.micronaut.http.annotation.PathVariable;
+import io.micronaut.http.annotation.QueryValue;
+
+@Controller("/cities")
+public class CitiesController {
+
+    @Get(produces = "application/json")
+    public HttpResponse<CityResponse> getByQuery(@QueryValue String name) {
+
+        return HttpResponse.ok();
+    }
+
+    @Get(produces = "application/json", value = "/{id}")
+    public HttpResponse<CityResponse> getById(@PathVariable String id) {
+
+        return HttpResponse.ok();
+    }
+}
---
+++ src/main/java/com/nomadcompass/infrastructure/request/CityResponse.java
@@ -0,0 +1,12 @@
+package com.nomadcompass.infrastructure.request;
+
+public class CityResponse {
+
+    private String id;
+    private String name;
+    private String state;
+    private String country;
+    private String latitude;
+    private String longitude;
+
+}
