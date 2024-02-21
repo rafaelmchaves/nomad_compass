170f5df3c78c5f1db73256c25f10335c48fd6909
Rafael Marques Chaves <rafaelmchaves@gmail.com>
2024-02-20 21:36:48 -0300

    Add getter and setter of lombok

--- src/main/java/com/nomadcompass/infrastructure/response/WeeklyCostResponse.java
+++ src/main/java/com/nomadcompass/infrastructure/response/WeeklyCostResponse.java
@@ -1,7 +1,12 @@
 package com.nomadcompass.infrastructure.response;
 
+import lombok.Getter;
+import lombok.Setter;
+
 import java.math.BigDecimal;
 
+@Getter
+@Setter
 public class WeeklyCostResponse {
 
     private String name;
