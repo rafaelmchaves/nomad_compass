a28aa22798b71788c1eddf3263231142f7abc6d8
Rafael Marques Chaves <rafaelmchaves@gmail.com>
2024-02-26 21:15:03 -0300

    Refactor the EstimationUseCaseImpl.
    Move all the bar cost rules to BarCost class model

--- src/main/java/com/nomadcompass/core/model/BarCost.java
+++ src/main/java/com/nomadcompass/core/model/BarCost.java
@@ -1,17 +1,47 @@
 package com.nomadcompass.core.model;
 
+import lombok.Builder;
 import lombok.Getter;
 import lombok.Setter;
 
 import java.math.BigDecimal;
+import java.math.RoundingMode;
 
+@Builder
 @Getter
 @Setter
 public class BarCost {
+
+    private static final Integer AVERAGE_ALCOHOL_DRINKS_PER_PERSON_BAR = 5;
+    private static final Integer AVERAGE_NO_ALCOHOL_DRINKS_PER_PERSON_BAR = 3;
 
     private BigDecimal alcoholDrink;
     private BigDecimal noAlcoholDrink;
     private BigDecimal cheapSnack;
     private BigDecimal expensiveSnack;
+
+    public BigDecimal getWeeklyCost(Integer amountDaysPerWeek, boolean isUseToEat, boolean isUseToDrinkAlcohol) {
+
+        BigDecimal barCostPerWeek = BigDecimal.ZERO;
+
+        if (amountDaysPerWeek > 0) {
+
+            if (isUseToDrinkAlcohol) {
+
+                barCostPerWeek = alcoholDrink.multiply(new BigDecimal(AVERAGE_ALCOHOL_DRINKS_PER_PERSON_BAR))
+                        .multiply(new BigDecimal(amountDaysPerWeek));
+
+            } else {
+                barCostPerWeek = noAlcoholDrink.multiply(new BigDecimal(AVERAGE_NO_ALCOHOL_DRINKS_PER_PERSON_BAR));
+            }
+
+            if (isUseToEat) {
+                final var averageSnack = (cheapSnack.add(expensiveSnack))
+                        .divide(BigDecimal.valueOf(2), RoundingMode.UP);
+                barCostPerWeek = barCostPerWeek.add(averageSnack.multiply(new BigDecimal(amountDaysPerWeek)));
+            }
+        }
+        return barCostPerWeek;
+    }
 
 }
--- src/main/java/com/nomadcompass/core/usecase/impl/EstimationUseCaseImpl.java
+++ src/main/java/com/nomadcompass/core/usecase/impl/EstimationUseCaseImpl.java
@@ -1,26 +1,24 @@
 package com.nomadcompass.core.usecase.impl;
 
+import com.nomadcompass.core.model.BarCost;
+import com.nomadcompass.core.model.LifeCost;
 import com.nomadcompass.core.usecase.EstimationUseCase;
 import com.nomadcompass.infrastructure.input.request.DestinationRequest;
 import com.nomadcompass.infrastructure.input.request.EstimationRequest;
 import com.nomadcompass.infrastructure.input.response.DestinationResponse;
 import com.nomadcompass.infrastructure.input.response.EstimationResponse;
 import com.nomadcompass.infrastructure.input.response.WeeklyCostResponse;
-import com.nomadcompass.infrastructure.output.entity.LifeCost;
 import com.nomadcompass.infrastructure.output.repository.LifeCostRepository;
 import jakarta.inject.Singleton;
 import lombok.RequiredArgsConstructor;
 
-import java.math.BigDecimal;
-import java.math.RoundingMode;
 import java.util.ArrayList;
 
 @RequiredArgsConstructor
 @Singleton
 public class EstimationUseCaseImpl implements EstimationUseCase {
 
-    private static final Integer AVERAGE_ALCOHOL_DRINKS_PER_PERSON_BAR = 5;
-    private static final Integer AVERAGE_NO_ALCOHOL_DRINKS_PER_PERSON_BAR = 3;
+
 
     private final LifeCostRepository lifeCostRepository;
 
@@ -30,11 +28,15 @@ public class EstimationUseCaseImpl implements EstimationUseCase {
         final EstimationResponse estimationResponse = new EstimationResponse();
         estimationResponse.setDestinations(new ArrayList<>());
         for (DestinationRequest destinationRequest: estimationRequest.getDestinations()) {
-            final var lifeCost = this.lifeCostRepository.findByCityId(destinationRequest.getCityId());
+            final var lifeCostEntity = this.lifeCostRepository.findByCityId(destinationRequest.getCityId());
 
+            final var lifeCost = getLifeCostModel(lifeCostEntity);
             WeeklyCostResponse weeklyCostResponse = new WeeklyCostResponse();
 
-            weeklyCostResponse.setBar(getBarCostPerWeek(estimationRequest, lifeCost));
+            final var weeklyBarCost = lifeCost.getBarCost().getWeeklyCost(estimationRequest.getAmountDaysPerWeekBar(),
+                    estimationRequest.isUseToEatBar(), estimationRequest.isUseToDrinkAlcohol());
+            weeklyCostResponse.setBar(weeklyBarCost);
+
             final var destinationResponse = new DestinationResponse();
             destinationResponse.setCityId(destinationResponse.getCityId());
             destinationResponse.setWeeklyCost(weeklyCostResponse);
@@ -45,29 +47,18 @@ public class EstimationUseCaseImpl implements EstimationUseCase {
         //transportation from each city to another(starting from root)
     }
 
-    private static BigDecimal getBarCostPerWeek(EstimationRequest estimationRequest, LifeCost lifeCost) {
-
-        BigDecimal barCostPerWeek = BigDecimal.ZERO;
-
-        if (estimationRequest.getAmountDaysPerWeekBar() > 0) {
-
-            if (estimationRequest.isUseToDrinkAlcohol()) {
-
-                barCostPerWeek = lifeCost.getBarCost().getAlcoholDrink()
-                        .multiply(new BigDecimal(AVERAGE_ALCOHOL_DRINKS_PER_PERSON_BAR))
-                        .multiply(new BigDecimal(estimationRequest.getAmountDaysPerWeekBar()));
-
-            } else {
-                barCostPerWeek = lifeCost.getBarCost().getNoAlcoholDrink().multiply(
-                        new BigDecimal(AVERAGE_NO_ALCOHOL_DRINKS_PER_PERSON_BAR));
-            }
-
-            if (estimationRequest.isUseToEatBar()) {
-                final var averageSnack = (lifeCost.getBarCost().getCheapSnack().add(lifeCost.getBarCost().getExpensiveSnack()))
-                        .divide(BigDecimal.valueOf(2), RoundingMode.UP);
-                barCostPerWeek = barCostPerWeek.add(averageSnack.multiply(new BigDecimal(estimationRequest.getAmountDaysPerWeekBar())));
-            }
-        }
-        return barCostPerWeek;
+    private LifeCost getLifeCostModel(com.nomadcompass.infrastructure.output.entity.LifeCost lifeCost) {
+        return LifeCost.builder()
+                .barCost(
+                        getBarCost(lifeCost.getBarCost())
+                )
+                .build();
     }
+
+    private static BarCost getBarCost(com.nomadcompass.infrastructure.output.entity.BarCost barCost) {
+        return BarCost.builder().alcoholDrink(barCost.getAlcoholDrink()).noAlcoholDrink(barCost.getNoAlcoholDrink())
+                .cheapSnack(barCost.getCheapSnack()).expensiveSnack(barCost.getExpensiveSnack())
+                .build();
+    }
+
 }
